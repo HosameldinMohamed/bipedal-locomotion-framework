1d51e6d5ca2dc3384492dfc5560ee00786ca3b18
Prashanth Ramadoss <prashanthramadoss@gmail.com>
2020-11-28 23:42:24 +0100

    [Perception] update ArucoDetector implementation

--- src/Perception/include/BipedalLocomotion/Perception/Features/ArucoDetector.h
+++ src/Perception/include/BipedalLocomotion/Perception/Features/ArucoDetector.h
@@ -12,9 +12,8 @@
 #include <BipedalLocomotion/System/Advanceable.h>
 
 #include <Eigen/Dense>
-#include <opencv2/aruco.hpp>
+#include <opencv2/core.hpp>
 
-#include <functional>
 #include <memory>
 #include <unordered_map>
 
@@ -31,7 +30,7 @@ struct ArucoMarkerData
     /**
      * Marker ID
      */
-    int id;
+    int id{-1};
     
     /**
      * Marker corners in camera coordinates
@@ -57,7 +56,7 @@ struct ArucoMarkerData
 struct ArucoDetectorOutput
 {
     std::unordered_map<int, ArucoMarkerData> markers;
-    double time_now{-1.0};
+    double timeNow{-1.0};
 };
 
 class ArucoDetector : public System::Advanceable<ArucoDetectorOutput>
@@ -69,23 +68,30 @@ public:
     /**
      * Initialize the detector
      * @note The following parameter are required by the filter:
-     * - "marker_dictionary" OpenCV predefined aruco marker dictionary
-     * - "marker_dictionary" marker length in m
+     * - "marker_dictionary" OpenCV predefined aruco marker dictionary 
+     * (available options: "4X4_50", "4X4_100", "4X4_250", "4X4_1000",
+     *                     "5X5_50", "5X5_100", "5X5_250", "5X5_1000",
+     *                     "6X6_50", "6X6_100", "6X6_250", "6X6_1000",
+     *                     "7X7_50", "7X7_100", "7X7_250", "7X7_1000",
+     *                     "ARUCO_ORIGINAL", 
+     *                     "APRILTAG_16h5", "APRILTAG_25h9", "APRILTAG_36h10","APRILTAG_36h11")
+     * options coherent with https://docs.opencv.org/master/d9/d6a/group__aruco.html#gac84398a9ed9dd01306592dd616c2c975
+     * - "marker_length" marker length in m
      * - "camera_matrix" 9d vector representing the camera calbration matrix in row major order
      * - "distortion_coefficients" 5d vector containing camera distortion coefficients
      * @param[in] handlerWeak weak pointer to a ParametersHandler::IParametersHandler interface
      * @tparameter Derived particular implementation of the IParameterHandler
      * @return True in case of success, false otherwise.
      */
-    bool initialize(std::weak_ptr<ParametersHandler::IParametersHandler> handlerWeak);
+    bool initialize(std::weak_ptr<ParametersHandler::IParametersHandler> handler);
         
     /**
      * Set image for which markers need to be detecte
      * @param[in] inputImg image as OpenCV mat
-     * @param[in] time_now current time in chosen time units
+     * @param[in] timeNow current time in chosen time units
      * @return True in case of success, false otherwise
      */
-    bool setImage(const cv::Mat& inputImg, double time_now);
+    bool setImage(const cv::Mat& inputImg, double timeNow);
 
     /**
      * Compute one step of the detector
@@ -105,15 +111,18 @@ public:
      * @param[in] markerData detected marker identifiers data
      * @return True in case of success, false if marker was not detected
      */
-    bool getArucoMarkerData(const int& id, ArucoMarkerData& markerData);
+    bool getDetectedMarkerData(const int& id, ArucoMarkerData& markerData);
     
     /**
      * Get the image with drawn detected markers
      * @param[in] outputImg image with detected markers drawn on it
      * @param[in] drawFrames draw also estimated marker poses, set to false by default
-     * @return True in case of success, false if no marker was not detected
+     * @param[in] axisLengthForDrawing axis length for drawing the frame axes, 0.1 by default
+     * @return True in case of success, false if no marker was detected
      */
-    bool getImgWithDetectedMarkers(cv::Mat& outputImg, bool drawFrames = false);
+    bool getImgWithDetectedMarkers(cv::Mat& outputImg,
+                                   const bool& drawFrames = false, 
+                                   const double& axisLengthForDrawing = 0.1);
 
     /**
      * Determines the validity of the object retrieved with get()
